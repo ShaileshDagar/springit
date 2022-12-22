d3dc4f685711433a419906b09beab6bdfacb71e6
Shailesh Dagar <dagar.shailesh@gmail.com>
2022-12-22 17:39:18 +0530

    The View Layer, but only with the Links

--- src/main/java/com/dagar/springit/SpringitApplication.java
+++ src/main/java/com/dagar/springit/SpringitApplication.java
@@ -4,17 +4,15 @@ import com.dagar.springit.domain.Comment;
 import com.dagar.springit.domain.Link;
 import com.dagar.springit.repository.CommentRepository;
 import com.dagar.springit.repository.LinkRepository;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.sql.SQLOutput;
-import java.util.List;
 import java.util.Optional;
 
 import static java.lang.System.out;
@@ -26,6 +24,11 @@ public class SpringitApplication {
 	private static final Logger logger = LoggerFactory.getLogger(SpringitApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
+	}
+
+	@Bean
+	PrettyTime prettyTime(){
+		return new PrettyTime();
 	}
 	//@Bean
 	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
